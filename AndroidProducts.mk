a02180392d6f188431b5b8dd6641d1dd3447ce1d
SirRGB <liam_wagner@gmx.de>
2021-12-14 16:20:23 +0100

    flounder: change sources to carbon

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -16,4 +16,4 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_flounder.mk \
-    $(LOCAL_DIR)/lineage_flounder.mk
+    $(LOCAL_DIR)/carbon_flounder.mk
--- carbon_flounder.mk
+++ carbon_flounder.mk
@@ -5,7 +5,7 @@ TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/carbon/config/common_tablet.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
@@ -36,6 +36,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2017-10-05
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_flounder
+PRODUCT_NAME := carbon_flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
